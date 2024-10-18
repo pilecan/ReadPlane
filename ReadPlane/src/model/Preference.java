e216c87057f63eaa2f4acc331754bbcc1ea79301
pilecan <pilecan@users.noreply.github.com>
2024-10-18 08:18:20 +0700

    new

---
+++ ReadPlane/src/model/Preference.java
@@ -0,0 +1,57 @@
+package model;
+
+public class Preference {
+	private String type;
+	private String path;
+	private String fileName;
+	private String key;
+	
+	
+	
+	public Preference( String key,String path) {
+		super();
+		this.path = path;
+		this.key = key;
+	}
+
+
+
+	public Preference() {
+		super();
+	}
+
+
+
+	public String getPath() {
+		return path;
+	}
+
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+
+
+	public String getKey() {
+		return key;
+	}
+
+
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Preference [type=" + type + ", path=" + path + ", fileName=" + fileName + ", key=" + key + "]";
+	}
+
+
+	
+
+}
