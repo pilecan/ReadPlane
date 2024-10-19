1fb2cc8d1ac5d80176b86c0d42a3d22e4acf107e
pilecan <pilecan@users.noreply.github.com>
2024-10-19 08:04:06 +0700

    new version

--- ReadPlane/src/util/Utility.java
+++ ReadPlane/src/util/Utility.java
@@ -4,21 +4,31 @@ import java.awt.Color;
 import java.awt.Desktop;
 import java.awt.Font;
 import java.awt.Image;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
-import com.ShowPlane;
+import com.ManageLivery;
 
 public class Utility {
 	private static Utility instance = new Utility();
 	private Properties prop;
+	private Properties prefs;
 
 	public static Utility getInstance() {
 		return instance;
@@ -26,8 +36,9 @@ public class Utility {
 
 	public void readProp() {
 
-	      try (InputStream input = ShowPlane.class.getResourceAsStream("/ressources/path.properties")) {
+	      try (InputStream input = ManageLivery.class.getResourceAsStream("/ressources/path.properties")) {
 
+	    	  System.out.println(input);
 	            this.prop = new Properties();
 
 	            // load a properties file
@@ -95,5 +106,66 @@ public class Utility {
 	public void setProp(Properties prop) {
 		this.prop = prop;
 	}
+
+	public Properties getPrefs() {
+		return prop;
+	}
+	
+	public void savePrefProperties() {
+		Writer out = null;
+		Path currentRelativePath = Paths.get("");
+		String file = currentRelativePath.toAbsolutePath().toString() + "/ressources/path.properties";
+		File f = new File(file);
+
+		try {
+			out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f), "utf-8"));
+			prefs.store(out, "ShowPlane Preferences");
+
+		} catch (IOException ex) {
+			System.err.println(ex.getMessage());
+		} finally {
+			try {
+				out.close();
+			} catch (Exception ex) {
+			}
+		}
+	}
+	
+	public void readPrefProperties() {
+
+		prefs = new Properties();
+
+		Path currentRelativePath = Paths.get("");
+		
+		System.out.println(ManageLivery.class.getResourceAsStream("/ressources/path.properties"));
+		String file = currentRelativePath.toAbsolutePath().toString() + "\\bin\\ressources\\path.properties";
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF8"));
+			prefs.load(is);
+			try {
+				is.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			System.err.println("Properties error " + e);
+		} catch (NullPointerException e) {
+			System.err.println("Properties null " + e);
+		}
+
+	}
+	
+	public String extractLastElement(String str){
+		return str.substring(str.lastIndexOf("\\")+1);
+	}
+	
+	public String extractLastPath(String str){
+		str = str.replace("\\", "/");
+		return str.substring(str.lastIndexOf("/")+1);
+	}
+	
+
+
+
 
 }
